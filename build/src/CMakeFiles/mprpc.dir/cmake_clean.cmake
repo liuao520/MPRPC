825ee41ca2aeb35fe7cc1d3c996555374eafa4c8
liuao520 <1147958934@qq.com>
2022-06-21 00:04:19 -0700

    first-

---
+++ build/src/CMakeFiles/mprpc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../lib/libmprpc.pdb"
+  "../../lib/libmprpc.so"
+  "CMakeFiles/mprpc.dir/mprpcapplication.cc.o"
+  "CMakeFiles/mprpc.dir/mprpcconfig.cc.o"
+  "CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mprpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
